cfcb72b2dc99aacdbe5a87796e22dc159be1d29d
David <david@pc-bd-dda.fi.unam>
2026-04-16 23:01:36 -0600

    Algoritmo FB funcional con metricas, función para imprimir la vista de los resultados

--- tareas/3/ArzateAdrian-DiazDavid/planificadore.py
+++ tareas/3/ArzateAdrian-DiazDavid/planificadore.py
@@ -173,6 +173,62 @@ def SPN(procesos):
         "Secuencia": secuencia
     }
 
+def FB(procesos, num_colas, quantum):
+    ticks = 0
+    secuencia = []
+
+    procesos_por_llegar = deque(procesos)
+    colas = [deque() for _ in range(num_colas)]
+
+    procesos_terminados = []
+    proceso_actual = None
+    nivel_actual = 0
+    quantum_restante = 0
+
+    while procesos_por_llegar or any(colas) or proceso_actual:
+        
+        while procesos_por_llegar and procesos_por_llegar[0].tiempo_llegada <= ticks:
+            colas[0].append(procesos_por_llegar.popleft())
+        
+        if not proceso_actual:
+            for i in range(num_colas):
+                if colas[i]:
+                    proceso_actual = colas[i].popleft()
+                    nivel_actual = i
+                    quantum_restante = quantum
+                    break
+        
+        if proceso_actual:
+            secuencia.append(proceso_actual.nombre)
+            proceso_actual.tiempo_restante -= 1
+            quantum_restante -= 1
+
+            if proceso_actual.tiempo_restante == 0:
+                tiempo_finalizacion = ticks + 1
+                
+                proceso_actual.tiempo_retorno = tiempo_finalizacion - proceso_actual.tiempo_llegada
+                proceso_actual.tiempo_espera = proceso_actual.tiempo_retorno - proceso_actual.tiempo_servicio
+                
+                procesos_terminados.append(proceso_actual)
+                proceso_actual = None
+            elif quantum_restante == 0:
+                siguiente_nivel = min(nivel_actual + 1, num_colas - 1)
+                colas[siguiente_nivel].append(proceso_actual)
+                proceso_actual = None
+        else:
+            secuencia.append('-')
+            
+        ticks += 1
+        
+    prom_T, prom_E, prom_P = calculo_metricas(procesos_terminados)
+
+    return {
+        "algoritmo": "FB",
+        "T": prom_T,
+        "E": prom_E,
+        "P": prom_P,
+        "Secuencia": secuencia
+    }
 
 def calculo_metricas(procesos_terminados):
     total_procesos = len(procesos_terminados)
@@ -184,8 +240,27 @@ def calculo_metricas(procesos_terminados):
 
     return (prom_T, prom_E, prom_P)
 
+def imprimir_vista(numero_ronda, procesos_originales, resultados):
+    print(f"- Ronda {numero_ronda}:")
+    
+    encabezado = "; ".join([f"{p.nombre}: {p.tiempo_llegada}, t={p.tiempo_servicio}" for p in procesos_originales])
+    suma_t = sum(p.tiempo_servicio for p in procesos_originales)
+    print(f"  {encabezado} (tot:{suma_t})")
+    
+    for res in resultados:
+        print(f"  {res['algoritmo']}: T={res['T']:.2f}, E={res['E']:.2f}, P={res['P']:.2f}")
+        esquema = "".join(res['Secuencia'])
+        print(f"  {esquema}")
+
+
+
 procesos_prueba = proceso_aleatorio(5)
 
-print(FCFS(copy.deepcopy(procesos_prueba)))
-print(RR(copy.deepcopy(procesos_prueba), 2))
-print(SPN(copy.deepcopy(procesos_prueba)))
+res_fcfs = FCFS(copy.deepcopy(procesos_prueba))
+res_rr   = RR(copy.deepcopy(procesos_prueba), 1) 
+res_spn  = SPN(copy.deepcopy(procesos_prueba))
+res_fb   = FB(copy.deepcopy(procesos_prueba, 3, 1))
+
+lista_resultados = [res_fcfs, res_rr, res_spn, res_fb]
+
+imprimir_vista(1, procesos_prueba, lista_resultados)
